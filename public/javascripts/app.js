12e39f06bee79284a6dc3c1bb0097a0035c2fe6e
Timur Morozov <radist@desant.biz>
2014-04-12 13:50:22 +0300

    data in table

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -71,4 +71,29 @@ angular.module("hto", [])
     $window.location.href = "/reports.html";
   };
 
+})
+.controller("ReportTableController", function($scope, ReportsLoader) {
+  $scope.table = {
+    header: [],
+    rows: []
+  };
+
+  ReportsLoader.loadReport(onReport);
+
+  function onReport(data) {
+    console.log(data);
+    $scope.table.rows = data;
+  }
+})
+.service("ReportsLoader", function($http) {
+  function loadReport(cb) {
+    $http.get("/api/zvit")
+    .success(function(data, status, headers, config) {
+      cb(data);
+    });
+  }
+
+  return {
+    loadReport: loadReport
+  }
 });
--- routes/api.js
+++ routes/api.js
@@ -1,10 +1,38 @@
 var express = require('express');
 var router = express.Router();
 
+
+var csv = require('csv');
+var fs = require('fs');
+
+
+
 /* GET users listing. */
 router.get('/zvit', function(req, res) {
-  var zvit = ["11","22", "33"];
-  res.json(zvit);
+  var rows = [];
+
+  csv()
+  .from.stream(fs.createReadStream(__dirname+'/../db/lvivska/truskavec/zvit.csv'))
+  .to(function(data){ console.log(data) })
+  .transform( function(row){
+    // row.unshift(row.pop());
+    return row;
+  })
+  .on('record', function(row,index){
+    console.log('#'+index+' '+JSON.stringify(row));
+    rows.push(row);
+  })
+  .on('end', function(count){
+    console.log('Number of lines: '+count);
+    //var rows = ["алібаба","джин", "лампа"];
+    res.json(rows);
+  })
+  .on('error', function(error){
+    console.log(error.message);
+  });
+
+
+
 });
 
 module.exports = router;
