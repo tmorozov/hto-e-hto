8f4a798b162d5e6395c66411592a4300bca5c766
Timur Morozov <radist@desant.biz>
2014-04-12 13:13:12 +0300

    api zvit entry

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ var bodyParser = require('body-parser');
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
-var zvit = require('./routes/zvit');
+var webApi = require('./routes/api');
 
 var app = express();
 
@@ -26,7 +26,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', routes);
 app.use('/users', users);
 
-app.use('/zvit', zvit);
+app.use('/api', webApi);
 
 /// catch 404 and forwarding to error handler
 app.use(function(req, res, next) {
--- routes/api.js
+++ routes/api.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res) {
+router.get('/zvit', function(req, res) {
   var zvit = ["11","22", "33"];
   res.json(zvit);
 });
